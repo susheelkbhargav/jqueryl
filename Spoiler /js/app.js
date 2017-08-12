945ac3cc3b6452fc2ab5deff7bc032fa33846d60
susheelkbhargav <susheelkbhargav@gmail.com>
2017-08-12 11:32:15 -0700

    Spoiler revealer using jquery

---
+++ Spoiler /js/app.js
@@ -0,0 +1,9 @@
+//Prevent spoilerphobes from seeing spoilers
+//Solution: Hide spoilers and reveal them through user ineraction
+
+$(".spoiler span").hide();
+$(".spoiler").append("<button type=button> reveal spoilers</button>");
+$(".spoiler button").click(
+   function(){$(".spoiler span").show();
+ $("button").remove();}
+	);
